d73963a196b80bbf3552599bfda10b0cb1c6de2e
Cara Steffen <cara@treadstonedesign.com>
2020-05-14 12:48:43 -0600

    converted App.js file into function component, added useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment, useEffect } from "react";
+import React, { useState, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
